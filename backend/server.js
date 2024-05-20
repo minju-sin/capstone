fba42cb51223cdd6741be3fd64e48262b487a4f7
chovi <sinminju04@gmail.com>
2024-05-20 13:39:48 +0900

    백엔드 + mysql 연동
    .env 파일은 등록안되게 함

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,22 @@
 // server.js
 
 const express = require('express');
-const cors = require("cors");
+const dbConnect = require('./config/dbConnect');
+const cors = require('cors');
 
 const app = express();
+dbConnect();
+
 app.use(express.json());
+
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true,
 }));
 
-app.post('/api/userData', (req, res) => {
-    console.log(req.body);
-    res.json('Data received');  // json 형태로 응답을 보냄
+// get 요청
+app.get('/api/userData', (req, res) => {
+    res.send(`hello`);
 });
 
 app.listen(8080, () => {		// 8080번 포트로 서버 실행
