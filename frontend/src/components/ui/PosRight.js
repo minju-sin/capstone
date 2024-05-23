0c12204845e0fb7bed1770904703385ee260366b
minju-sin <sinminju04@gmail.com>
2024-05-23 16:40:13 +0900

    주석설명 추가
    다음은 신용카드 결제화면 구현 및 결제내역 db 추가

--- frontend/src/components/ui/PosRight.js
+++ frontend/src/components/ui/PosRight.js
@@ -1,25 +1,35 @@
 // ./PosRight.js
 
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { StyledRightHalf, StyledButtonContainer, StyledRightButton, StyledButtonMerged, StyledA } from "../styles/PosRightCSS";
 import Button from "./Button";
 
 
 function PosRight({ addOrder, handleCashButtonClick   }) {
     
-    const [menuItems, setMenuItems] = useState([]);
+    const [menuItems, setMenuItems] = useState([]); // 메뉴판 상태
 
+    // 컴포넌트가 마운트될 때 한 번만 실행되는 useEffect 훅을 사용하여 데이터를 가져옴
     useEffect(() => {
-        fetchMenuItems();
+        fetchMenuItems(); // fetchMenuItems 함수 호출
     }, []);
 
+    // 서버에서 메뉴 아이템 데이터를 가져오는 비동기 함수
     const fetchMenuItems = async () => {
         try {
+            // 서버로 GET 요청을 보내고 응답을 받음
             const response = await fetch('http://localhost:8080/menu');
+
+            // 응답 데이터를 JSON 형식으로 변환
             const data = await response.json();
+
+            // 메뉴 아이템 상태를 업데이트
             setMenuItems(data);
+
         } catch (error) {
-            console.error('Error fetching menu items:', error);
+            // 에러가 발생한 경우 콘솔에 에러 메시지를 출력
+            console.error('에러가 발생했습니다.', error);
         }
     };
 
